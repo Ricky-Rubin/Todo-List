6b20ee7885a4fe85af32bf667c04467236d0ead2
Ricky-Rubin <owolabi1738@gmail.com>
2025-03-03 03:12:23 +0100

    Updated project tabs to allow a second click which activates the "Add Task" button display.

--- src/todo.js
+++ src/todo.js
@@ -42,13 +42,42 @@ function fillContent() {
     addReminder.setAttribute("id", "addReminder");
     addReminder.setAttribute("required", "");
 
+    const dateLabel = document.createElement('label');
+    dateLabel.textContent = "Choose a date: "
+    dateLabel.setAttribute("for", "date");
+    dateLabel.className = 'class-date';
+
+    const dateTab = document.createElement('input');
+    dateTab.className = 'date-class';
+    dateTab.setAttribute("type", "date");
+    dateTab.setAttribute("id", "date");
+
+    const selectLabel = document.createElement('label');
+    selectLabel.setAttribute("for", "selectOption");
+    selectLabel.textContent = "Select urgency level: ";
+    selectLabel.className = 'select-label'
+
+    const selectOptions = document.createElement('select'); //Urgency select options
+    selectOptions.className = 'select-options';
+    selectOptions.setAttribute("id", "selectOption");
+    selectOptions.innerHTML = `
+        <option default disabled>Choose</option>
+        <option>Low</option>
+        <option>Medium</option>
+        <option>High</option>
+    `;
+
+    const sendBtn = document.createElement('div'); //submit button for the task details__slide form.
+    sendBtn.className = 'send-btn'
+    sendBtn.innerHTML= `<button class='add-btn'>Add</button>`
+
     const topTextTwo = document.createElement('p'); //This is the text that will show up at the top
     topTextTwo.className = 'top-text-two';          // of the slide to fill in the todo task details
     topTextTwo.textContent = 'Todo Details';
 
     const topTextOne = document.createElement('div');
     topTextOne.className = 'top-text-one';   //This is the Todo text that will show on the landing
-    topTextOne.textContent = 'Todo';        // It will be at the top of teh left pane.
+    topTextOne.textContent = 'Todo';        // It will be at the top of the left pane.
 
     const addProject = document.createElement('button'); //Button to create new project
     addProject.className = 'project'                  
@@ -62,35 +91,67 @@ function fillContent() {
         projectTab.innerHTML = `
             <input class='p-name' type='text'/>
             <button class='btn mark'>&#10004;</button>
-            <button class='btn cancel'>&#10006;</button>
-        `;
+            <button class='btn cancel'>&#10006;</button>  
+        `;                                                   //cancel button for the project name
 
         navPane.appendChild(projectTab);
 
         const pName = projectTab.querySelector('.p-name');
-
+        pName.focus();
+        const markBtn = projectTab.querySelector('.mark');
         const cancelBtn = projectTab.querySelector('.cancel');
+
         cancelBtn.addEventListener('click', () => {
-            navPane.removeChild(projectTab)
+            navPane.removeChild(projectTab);
         });
 
-        const markBtn = projectTab.querySelector('.mark');
-        markBtn.addEventListener('click', () => {
-            projectTab.innerHTML = ""
-            projectTab.style.backgroundColor = 'pink';
-            projectTab.style.padding = '0px 8px'
-            projectTab.style.fontSize = '14px'
-            projectTab.style.fontWeight = '600'
-            
-            projectTab.innerHTML += `
-                <p>${pName.value}</p>
-                <button class='project-cancel'>&#10006;</button>
-            `;
+        function setProjectName() {
+            if (pName.value.trim() !== "") {
+                projectTab.innerHTML = ""
+                projectTab.style.backgroundColor = 'pink';
+                projectTab.style.padding = '0px 8px'
+                projectTab.style.fontSize = '14px'
+                projectTab.style.fontWeight = '600'
 
-            const projectCancel = projectTab.querySelector('.project-cancel');
-            projectCancel.addEventListener('click', () => {
-                projectTab.remove();
-            })
+                projectTab.innerHTML += `
+                <div class='tab-house'>
+                    <p>${pName.value}</p>
+                </div>
+
+                <div class='cancel-house'>
+                    <button class='project-cancel'>&#10006;</button>
+                </div>
+                `; 
+
+                const tabCancel = projectTab.querySelector('.project-cancel');
+                tabCancel.addEventListener('click', () => {
+                    if (slideForm.style.right = '0') {
+                        slideForm.style.right = '-330px'
+                    }
+                })
+
+                const tabHouse = projectTab.querySelector('.tab-house');
+                    tabHouse.addEventListener('click', () => {
+                    console.log("div clicked");
+                    projectTab.style.backgroundColor = "#dff9fb";
+                    slideButton.style.display = 'block';
+                })
+
+                const projectCancel = projectTab.querySelector('.cancel-house');
+                projectCancel.addEventListener('click', () => {
+                    projectTab.remove();
+                    slideButton.style.display = 'none'
+                })
+            } else {
+                alert ("You need to fill the input section!")
+            };
+        };
+
+        markBtn.addEventListener('click', setProjectName);
+        pName.addEventListener('keydown', (e) => {
+            if (e.key === "Enter") {
+                setProjectName();
+            }
         })
     })
 
@@ -99,14 +160,14 @@ function fillContent() {
     topName.innerHTML = `
         <p class='top-task'>Tasks: </p>
         <button class='add-task'>Add Task</button>
-    `;
-
+    `;                   
+     
     contentPane.append(topName);
 
     const slideForm = document.createElement('div');
     slideForm.className = 'slide-form';
 
-    contentPane.appendChild(slideForm)
+    contentPane.appendChild(slideForm);
 
     const slideButton = topName.querySelector('.add-task');
     slideButton.addEventListener('click', () => {
@@ -124,8 +185,34 @@ function fillContent() {
         slideForm.style.right = '-300px'
     })
 
-    formHouse.append(titleLabel, addTitle, descLabel, addDesc, reminderLabel, addReminder);
+    formHouse.append(topTextTwo, titleLabel, addTitle, descLabel, addDesc, reminderLabel, addReminder, dateLabel, dateTab, selectLabel, selectOptions, sendBtn);
     slideForm.appendChild(formHouse)
+
+    sendBtn.addEventListener('click', () => {
+        const newTask = new Task(addTitle.value, addDesc.value, addReminder.value);
+        console.log(newTask);
+
+        const todoDiv = document.createElement('div');
+        todoDiv.className = 'todo-div';
+        todoDiv.setAttribute("id", "todo-div");
+
+        const titleArea = document.createElement('p');
+        titleArea.textContent = newTask.title;
+
+        const descArea = document.createElement('p');
+        descArea.textContent = newTask.description;
+
+        const reminderArea = document.createElement('p');
+        reminderArea.textContent = newTask.reminder;
+
+        todoTab.append(titleArea, descArea, reminderArea);
+
+        const contentArea = document.createElement('div')
+        contentArea.className = 'content-area'
+        contentPane.appendChild(contentArea);
+
+        contentArea.appendChild(todoTab);
+    })
 }
 
 
